a239c3770d686ddfe32c8e5c491f81c3aafc5796
aarchi123-34 <Aarchi.Miglani@ibm.com>
2026-02-17 02:17:50 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,4 +1,12 @@
-variable "key_name" {
-  description = "Name of SSH key pair"
-  type        = string
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  # Ubuntu 22.04 LTS AMI
+  default = "ami-0a313d6098716f372"
 }
