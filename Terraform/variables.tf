c6096c725f031ba50e9038be0e6f9576892940a7
aarchi123-34 <Aarchi.Miglani@ibm.com>
2026-02-17 02:46:19 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,12 +1,3 @@
 variable "region" {
   default = "us-east-1"
 }
-
-variable "instance_type" {
-  default = "t3.micro"
-}
-
-variable "ami_id" {
-  # Ubuntu 22.04 LTS AMI
-  default = "ami-0b6c6ebed2801a5cb"
-}
