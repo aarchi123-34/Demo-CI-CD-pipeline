b81ff473b2867a9da3998b583404180bca2037ff
aarchi123-34 <Aarchi.Miglani@ibm.com>
2026-02-17 02:17:00 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,17 +1,35 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-resource "aws_instance" "ubuntu_ec2" {
-  ami           = "ami-08c40ec9ead489470"  
-  instance_type = "t3.micro"
-  key_name      = var.key_name
 
-  tags = {
-    Name = "demoEC2"
+resource "aws_security_group" "ssh_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.ubuntu_ec2.public_ip
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = "my-key-pair"
+  security_groups = [aws_security_group.ssh_sg.name]
+
+  tags = {
+    Name = "Terraform-Ansible-EC2"
+  }
 }
